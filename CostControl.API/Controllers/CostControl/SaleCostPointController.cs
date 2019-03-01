b55095e67fa9fc9090d639b5edfcad6ba10ed80e
HRFakhri <Hamid_Reza_Fakhri@yahoo.com>
2019-03-01 23:09:52 +0330

    Remove unused .js files

--- CostControl.API/Controllers/CostControl/SaleCostPointController.cs
+++ CostControl.API/Controllers/CostControl/SaleCostPointController.cs
@@ -1,12 +1,43 @@
 ﻿using CostControl.API.Controllers.Base;
+using CostControl.API.Models;
 using CostControl.BusinessEntity.Models.CostControl;
 using CostControl.BusinessLogic.Logics.CostControl;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 
 namespace CostControl.API.Controllers.CostControl
 {
     [Route("api/[controller]")]
     [ApiController]
     [Produces("application/json")]
-    public class SaleCostPointController : BaseApiController<SaleCostPoint, SaleCostPointLogic, long> { }
+    public class SaleCostPointController : BaseApiController<SaleCostPoint, SaleCostPointLogic, long>
+    {
+        [HttpGet("Get")]
+        public override ActionResult<ServiceResponse<SaleCostPoint>> Get([FromQuery]Pagination paginate = null, string token = "")
+        {
+            try
+            {
+                paginate = (paginate == null || paginate.PageSize <= 0) ? new Pagination() : paginate;
+                paginate.RowCount = PDKBusinessLogic.GetCount();
+
+                return GenerateResponse(paginate,
+                                    PDKBusinessLogic.Get(
+                                        includeProperties: new List<Expression<Func<IQueryable<SaleCostPoint>, IIncludableQueryable<SaleCostPoint, object>>>>{
+                                                a => a.Include(b => b.SalePoint),
+                                                a => a.Include(b => b.CostPoint)
+                                        },
+                                        pageSize: paginate.PageSize,
+                                        page: paginate.PageNumber));
+            }
+            catch (Exception e)
+            {
+                return GenerateExceptionResponse(e, "Exception!");
+            }
+        }
+    }
 }
--- CostControl.BusinessEntity/Models/CostControl/SaleCostPoint.cs
+++ CostControl.BusinessEntity/Models/CostControl/SaleCostPoint.cs
@@ -14,14 +14,18 @@ namespace CostControl.BusinessEntity.Models.CostControl
         [Display(Name = "مرکز فروش")]
         public long SalePointId { get; set; }
 
+        [Display(Name = "نام مرکز فروش")]
+        public string SalePointName { get; set; }
+
+        public SalePoint SalePoint { get; set; }
+
         [Required(ErrorMessage = "مرکز هزینه اجباریست!")]
         [Display(Name = "مرکز هزینه")]
         public long CostPointId { get; set; }
         
-        [Display(Name = "نام مرکز فروش")]
-        public string SalePointName { get; set; }
-
         [Display(Name = "نام مرکز هزینه")]
         public string CostPointName { get; set; }
+
+        public CostPoint CostPoint { get; set; }
     }
 }
